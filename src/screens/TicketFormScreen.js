50ee8c9af323e173926a06d924e535e2d1a2fb10
DiMaPaGa <145446082+DiMaPaGa@users.noreply.github.com>
2025-05-20 17:08:20 +0200

    Cambio título imagen

--- src/screens/TicketFormScreen.js
+++ src/screens/TicketFormScreen.js
@@ -148,7 +148,7 @@ const uploadImageToCloudinary = async (uri) => {
             {imageUri ? (
             <Image source={{ uri: imageUri }} style={styles.imageIcon} />
             ) : (
-            <Image source={require('../../assets/images/addpubb.png')} style={styles.imageIcon} />
+            <Image source={require('../../assets/images/addpubB.png')} style={styles.imageIcon} />
             )}
         </TouchableOpacity>
 
